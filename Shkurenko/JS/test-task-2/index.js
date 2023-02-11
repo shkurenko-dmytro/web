e48c21aca1abd9c6eb9bf01ad511022010847d0e
shkurenko dmytro <dima.shkurenko.02@gmail.com>
2023-02-11 22:29:35 +0200

    test work with json

--- Shkurenko/JS/test-task-2/index.js
+++ Shkurenko/JS/test-task-2/index.js
@@ -1,13 +1,58 @@
 
-const output = document.querySelector('div');
-const requestURL = 'example_2.json';
+const output = document.querySelector('table');
+const tableTr = document.querySelector('table thead tr');
+const tableTbody = document.getElementsByTagName('td');
+const requestURL = 'index.json';
+const searchBtn = document.querySelector('.search-block__button');
 
-/////////////////////////////
-
-let a = fetch(requestURL).then(response => response.json());
-console.log(a);
-a.then(data => {
-    console.log(data.quiz.sport.q1.question);
-    output.innerHTML = data.quiz.sport.q1.question;
-    output.innerHTML += data.quiz.sport.q1.options[0];
+///////////////////////////// Tesr show all data table   /////////////////////////////
+searchBtn.addEventListener('click', () => {
+        if(output.childNodes.length <=5)renderAll();
 });
+
+let value = 'Vikings';
+let key = 'Title';
+
+function filterBySelected(  _key , value ) {
+    let a = fetch(requestURL).then(response => response.json());
+    a.then(data => {
+        let b = data.filter(item => item[_key] == value);
+        console.log( 'filterBySelected ' ,b);
+    });
+};
+function sortByYear() {
+    let a = fetch(requestURL).then(response => response.json());
+    a.then(data => {
+        let b = data.sort((a, b) => a.Year - b.Year);
+        console.log('sortByYear' ,b);
+    });
+}
+function renderAll() {
+    let a = fetch(requestURL).then(response => response.json());
+    a.then(data => {
+        data.forEach(item => {
+            let tr = document.createElement('tr');
+            tr.innerHTML = `
+                <td>${item.Title}</td>
+                <td>${item.Year}</td>
+                <td>${item.Runtime}</td>
+            `;
+            output.append(tr);
+        });
+        
+        });
+        
+}
+
+function sortByTitle() {
+    let a = fetch(requestURL).then(response => response.json());
+    a.then(data => {
+        function SortArray(x, y){
+            return x.Title.localeCompare(y.Title);
+        }
+        let b = data.sort(SortArray);
+        console.log('sortByTitle', b, );
+    });
+}
+
+
