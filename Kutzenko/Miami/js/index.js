a5dbe4d5f231df1316b4184f5ba4967b0ebf8c9e
Maxim Kuts <makskutss@gmail.com>
2023-02-20 15:56:33 +0200

    Miami: menu-burger, navigation on page

--- Kutzenko/Miami/js/index.js
+++ Kutzenko/Miami/js/index.js
@@ -31,9 +31,44 @@ if(isMobile.any()) {
 
   //Вивід номера телефону
   const phoneIcon = document.querySelector('#phone');
-  phoneIcon.addEventListener('click', function(event){
+  phoneIcon.addEventListener('click', function(){
     phoneIcon.classList.toggle('_active');
   })
 }else{
   document.body.classList.add('_pc');
+}
+//Прокрутка
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if(menuLinks.length > 0){
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener('click', onMenuLinksClick);
+    });
+
+    function onMenuLinksClick(e){
+        const menuLink = e.target;
+        if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset;
+
+            if(iconMenu.classList.contains('_active')){
+                iconMenu.classList.remove('_active');
+                menuBody.classList.remove('_active');
+            }
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: 'smooth'
+            });
+            e.preventDefault();
+        }
+    }
+}
+//Бургер
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if(iconMenu){
+    iconMenu.addEventListener('click', function(){
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    })
 }
