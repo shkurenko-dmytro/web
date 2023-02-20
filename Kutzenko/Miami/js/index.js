260081d661ebe6f9b12abfdb288e49316e03d213
Maxim Kuts <makskutss@gmail.com>
2023-02-20 14:42:41 +0200

    Miami: fix phone output

---
+++ Kutzenko/Miami/js/index.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    Blackberry: function() {
+        return navigator.userAgent.match(/Blackberry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (
+            isMobile.Android() ||
+            isMobile.Blackberry() ||
+            isMobile.iOS() ||
+            isMobile.Opera() ||
+            isMobile.Windows());
+    }
+};
+
+if(isMobile.any()) {
+  document.body.classList.add('_touch');
+
+  //Вивід номера телефону
+  const phoneIcon = document.querySelector('#phone');
+  phoneIcon.addEventListener('click', function(event){
+    phoneIcon.classList.toggle('_active');
+  })
+}else{
+  document.body.classList.add('_pc');
+}
