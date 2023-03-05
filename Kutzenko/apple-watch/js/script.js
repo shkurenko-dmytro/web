a43a8e4af3e64fa0ce43eb3975837017df30cf2e
Maxim Kuts <makskutss@gmail.com>
2023-03-05 16:15:11 +0200

    a-w: current slide, work with bulet, change bg

--- Kutzenko/apple-watch/js/script.js
+++ Kutzenko/apple-watch/js/script.js
@@ -1,4 +1,4 @@
-new Swiper('.my-swiper', {
+let mySlider = new Swiper('.my-swiper', {
   //Стрілки
   navigation: {
     nextEl: '.swiper-btn-next',
@@ -10,4 +10,40 @@ new Swiper('.my-swiper', {
     // Буллети
     clickable: true,
   },
+  keyboard: {
+    enabled: true,
+  },
 });
+
+//Поточний слайд
+let slideCount = document.querySelector('.my-swiper__number');
+mySlider.on('slideChange', function(){
+  let currentSlide = ++mySlider.realIndex;
+  slideCount.innerText = currentSlide;
+})
+
+//Зміна кольору фону
+let pageNode = document.querySelector('.page');
+let activeLinkHeader = document.querySelector('.active-link')
+mySlider.on('slideChange', function(){
+  switch (mySlider.realIndex){
+    case 1:
+      pageNode.style.background = 'linear-gradient(89.83deg, #404356 0.17%, rgba(192, 213, 224, 0) 165.01%)'
+      activeLinkHeader.style.color = 'rgba(66, 69, 88, 1)'
+      break
+    case 2:
+      pageNode.style.background = 'linear-gradient(89.83deg, #C0D5E0 0.17%, rgba(192, 213, 224, 0) 165.01%)'
+      activeLinkHeader.style.color = 'rgba(198, 219, 229, 1)'
+      break
+    case 3:
+      pageNode.style.background = 'linear-gradient(89.83deg, #78DCD0 0.17%, rgba(192, 213, 224, 0) 165.01%)'
+      activeLinkHeader.style.color = 'rgba(121, 220, 208, 1)'
+      break
+    case 4:
+      pageNode.style.background = 'linear-gradient(89.83deg, #F0C7C1 0.17%, rgba(192, 213, 224, 0) 165.01%)'
+      activeLinkHeader.style.color = 'rgba(240, 199, 193, 1)'
+      break
+    default:
+      break
+  }
+})
