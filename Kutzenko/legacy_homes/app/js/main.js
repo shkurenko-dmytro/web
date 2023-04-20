4b19b6ccda598657b3949302cff4e403db2d67c0
Maxim Kuts <makskutss@gmail.com>
2023-04-20 16:21:03 +0300

    legacy: partner-swiper-buttons hidden

--- Kutzenko/legacy_homes/app/js/main.js
+++ Kutzenko/legacy_homes/app/js/main.js
@@ -17,8 +17,8 @@ const swiper = new Swiper('.testimonials__swiper', {
 
   autoHeight: true,
 
-  breakpoints:{
-    769:{
+  breakpoints: {
+    769: {
       slidesPerView: 1.5,
       initialSlide: 1
     }
@@ -28,25 +28,35 @@ const swiper = new Swiper('.testimonials__swiper', {
 const testimonialsButtonPrev = document.querySelector('.testimonials__swiper-button-prev');
 const testimonialsButtonNext = document.querySelector('.testimonials__swiper-button-next');
 
-testimonialsButtonPrev.addEventListener('click', function(){
-  if(swiper.slides[0].classList.contains('swiper-slide-active')){
+if (swiper.slides[0].classList.contains('swiper-slide-active')) {
+  testimonialsButtonPrev.style.opacity = '0';
+  testimonialsButtonPrev.style.visibility = 'hidden';
+}
+
+if (swiper.slides[swiper.slides.length - 1].classList.contains('swiper-slide-active')) {
+  testimonialsButtonNext.style.opacity = '0';
+  testimonialsButtonNext.style.visibility = 'hidden';
+}
+
+testimonialsButtonPrev.addEventListener('click', function () {
+  if (swiper.slides[0].classList.contains('swiper-slide-active')) {
     testimonialsButtonPrev.style.opacity = '0';
     testimonialsButtonPrev.style.visibility = 'hidden';
   }
 
-  if(!swiper.slides[swiper.slides.length - 1].classList.contains('swiper-slide-active')){
+  if (!swiper.slides[swiper.slides.length - 1].classList.contains('swiper-slide-active')) {
     testimonialsButtonNext.style.opacity = '1';
     testimonialsButtonNext.style.visibility = 'visible';
   }
 })
 
-testimonialsButtonNext.addEventListener('click', function(){
-  if(!swiper.slides[0].classList.contains('swiper-slide-active')){
+testimonialsButtonNext.addEventListener('click', function () {
+  if (!swiper.slides[0].classList.contains('swiper-slide-active')) {
     testimonialsButtonPrev.style.opacity = '1';
     testimonialsButtonPrev.style.visibility = 'visible';
   }
 
-  if(swiper.slides[swiper.slides.length - 1].classList.contains('swiper-slide-active')){
+  if (swiper.slides[swiper.slides.length - 1].classList.contains('swiper-slide-active')) {
     testimonialsButtonNext.style.opacity = '0';
     testimonialsButtonNext.style.visibility = 'hidden';
   }
@@ -70,6 +80,42 @@ const swiper2 = new Swiper('.partners__swiper', {
   slidesPerView: 1,
 });
 
+const partnersButtonPrev = document.querySelector('.partners__button-prev');
+const partnersButtonNext = document.querySelector('.partners__button-next');
+
+if (swiper2.slides[0].classList.contains('swiper-slide-active')) {
+  partnersButtonPrev.style.opacity = '0.2';
+  partnersButtonPrev.style.pointerEvents = 'none';
+}
+
+if (!swiper2.slides[0].classList.contains('swiper-slide-active')) {
+  partnersButtonPrev.style.opacity = '1';
+  partnersButtonPrev.style.pointerEvents = 'all';
+}
+
+partnersButtonPrev.addEventListener('click', function () {
+  if (swiper2.slides[0].classList.contains('swiper-slide-active')) {
+    partnersButtonPrev.style.opacity = '0.2';
+    partnersButtonPrev.style.pointerEvents = 'none';
+  }
+
+  if (!swiper2.slides[swiper2.slides.length - 1].classList.contains('swiper-slide-active')) {
+    partnersButtonNext.style.opacity = '1';
+    partnersButtonNext.style.pointerEvents = 'all';
+  }
+})
+
+partnersButtonNext.addEventListener('click', function () {
+  if (!swiper2.slides[0].classList.contains('swiper-slide-active')) {
+    partnersButtonPrev.style.opacity = '1';
+    partnersButtonPrev.style.pointerEvents = 'all';
+  }
+
+  if (swiper2.slides[swiper2.slides.length - 1].classList.contains('swiper-slide-active')) {
+    partnersButtonNext.style.opacity = '0.2';
+    partnersButtonNext.style.pointerEvents = 'none';
+  }
+})
 
 const isMobile = {
   Android: function () {
