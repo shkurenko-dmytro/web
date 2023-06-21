379401a2bbe2f33416bba6eddca95cf71c0ce250
shkurenko dmytro <dima.shkurenko.02@gmail.com>
2023-06-21 23:02:49 +0300

    test server

--- Shkurenko/Project`s/NodeJS/index.js
+++ Shkurenko/Project`s/NodeJS/index.js
@@ -12,7 +12,9 @@ app.get('/', (req, res) => {
 app.get('/objects', (req, res) => {
     res.sendFile(path.resolve(__dirname,'../../../Kutzenko/legacy_homes/app/objects.html'))
 })
-
+app.post('/post',(req, res) => {
+    res.status(201).send('4jng4g4')
+})
 app.listen(PORT,() => {
     console.log(`Server has been started on port ${PORT}...`);
 })
