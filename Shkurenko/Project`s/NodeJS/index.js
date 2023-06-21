e8d78c6ac19c575534f3663b8f95158d176eb621
shkurenko dmytro <dima.shkurenko.02@gmail.com>
2023-06-21 14:38:22 +0300

    test server

--- Shkurenko/Project`s/NodeJS/index.js
+++ Shkurenko/Project`s/NodeJS/index.js
@@ -1,26 +1,21 @@
 const express  = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs')
 
 const PORT = 5000;
-app.use(express.static( path.resolve (__dirname,'YourSneaker')))
-//app.use(express.static('YourSneaker/less6'))
+app.use(express.static( path.resolve(__dirname,'../../../Kutzenko/legacy_homes/app')))
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.resolve(__dirname,'YourSneaker', 'index.html'))
-// })
-// app.get('/two', (req, res) => {
-//     res.sendFile(path.resolve(__dirname,'YourSneaker','less6', 'index.html'))
-// })
-// app.get('/download', (req, res) => {
-//     res.download(path.resolve(__dirname,'YourSneaker','a.txt'))
-// })
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'../../../Kutzenko/legacy_homes/app/index.html'))
+})
+app.get('/objects', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'../../../Kutzenko/legacy_homes/app/objects.html'))
+})
 
 app.listen(PORT,() => {
     console.log(`Server has been started on port ${PORT}...`);
 })
-
-
 
 
 
