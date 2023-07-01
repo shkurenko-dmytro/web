591fb04ca509f78ad1a18a8129012b331dbfb608
Maxim Kuts <makskutss@gmail.com>
2023-07-01 13:13:45 +0300

    account page, add Sign Up form

--- Kutzenko/Tesla/app/js/account.js
+++ Kutzenko/Tesla/app/js/account.js
@@ -1,59 +1,121 @@
 'use strict'
 
 if(document.querySelector('.form')){
-  const form = document.querySelector('.form');
+  const formContainer = document.querySelector('.container-form');
+
+  const formLogin = document.querySelector('.form__login');
+  const formSignUp = document.querySelector('.form__sign-up');
+
   const username = document.querySelector('#username');
   const password = document.querySelector('#password');
-  const username_err = document.querySelector('.form__username-error');
-  const password_err = document.querySelector('.form__password-error');
+  const usernameError = document.querySelector('#username-error');
+  const passwordError = document.querySelector('#password-error');
 
-  username.addEventListener('textInput', username_Verify);
-  password.addEventListener('textInput', password_Verify);
+  const usernameSignUp = document.querySelector('#username-signUp');
+  const passwordSignUp = document.querySelector('#password-signUp');
+  const confirmPassword = document.querySelector('#confirm-password');
+  const usernameSignUpError = document.querySelector('#username-signUp-error');
+  const passwordSignUpError = document.querySelector('#password-signUp-error');
+  const confirmPasswordError = document.querySelector('#confirm-password-error');
 
-  function validated(){
-    if(username.value.length < 4) {
-      username.style.borderColor = "red";
-      username_err.style.display = "block";
-      username.focus();
+  const linkToSignUp = document.querySelector('#link-to-signUp');
+  const linkToLogin = document.querySelector('#link-to-login');
+
+  username.addEventListener('input', username_Verify);
+  password.addEventListener('input', password_Verify);
+
+  usernameSignUp.addEventListener('input', username_Verify);
+  passwordSignUp.addEventListener('input', password_Verify);
+  confirmPassword.addEventListener('input', confirmPassword_Verify);
+
+  function validated(name, pass, nameError, passError, confirm_pass = null, confirm_passError = null){
+    if(name.value.length < 4) {
+      name.style.borderColor = "red";
+      nameError.style.display = "block";
+      name.focus();
 
       return false;
     }
 
-    if(password.value.length < 5) {
-      password.style.borderColor = "red";
-      password_err.style.display = "block";
-      password.focus();
+    if(pass.value.length < 4) {
+      pass.style.borderColor = "red";
+      passError.style.display = "block";
+      pass.focus();
 
       return false;
+    }
+  
+    if(confirm_pass){
+      if(confirm_pass.value !== pass.value){
+        confirm_pass.style.borderColor = "red";
+        confirm_passError.style.display = "block";
+        confirm_pass.focus();
+
+        return false;
+      }
     }
 
     return true;
   }
 
-  function username_Verify(){
-    if(username.value.length >= 3){
-      console.log(username.value.length)
-      username.style.borderColor = "gray";
-      username_err.style.display = "none";
+  function username_Verify(e){
+    if(e.target.value.length >= 4){
+      e.target.style.borderColor = "gray";
+      usernameError.style.display = "none";
+      usernameSignUpError.style.display = "none";
 
       return true;
     }
   }
 
-  function password_Verify(){
-    if(password.value.length >= 4){
-      password.style.borderColor = "gray";
-      password_err.style.display = "none";
+  function password_Verify(e){
+    if(e.target.value.length >= 4){
+      e.target.style.borderColor = "gray";
+      passwordError.style.display = "none";
+      passwordSignUpError.style.display = "none";
 
       return true;
     }
   }
 
-  form.addEventListener('submit', (e) => {
+  function confirmPassword_Verify(e){
+    if(e.target.value === passwordSignUp.value){
+      e.target.style.borderColor = "gray";
+      confirmPasswordError.style.display = "none";
+
+      return true;
+    }
+  }
+
+  //Перехід між авторизацією та регістрацією
+  linkToSignUp.addEventListener('click', (e) => {
     e.preventDefault();
 
-    
-    if(validated()){
+    formContainer.classList.add('active');
+  })
+
+  linkToLogin.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    formContainer.classList.remove('active');
+  })
+
+  //Авторизація
+  formLogin.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if(validated(username, password, usernameError, passwordError)){
+      alert('Летс гоу всі');
+    } else {
+      return false;
+    }
+  })
+
+  //Регістрація
+  formSignUp.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if(validated(usernameSignUp, passwordSignUp, usernameSignUpError, passwordSignUpError, confirmPassword, confirmPasswordError)){
       alert('Летс гоу всі');
     } else {
       return false;
