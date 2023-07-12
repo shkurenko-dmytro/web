6c2332b133f2f1ca831e3d8b2c79c03816e4a99f
Maxim Kuts <makskutss@gmail.com>
2023-07-12 18:06:57 +0300

    update sign-up

--- Kutzenko/Tesla/src/js/account.js
+++ Kutzenko/Tesla/src/js/account.js
@@ -5,26 +5,29 @@ import addMenuClass from './modules/menu';
 import urlList from './modules/urlList';
 
 const preloader = document.querySelector('.preloader');
+const reqURL = urlList.localServer;
 
-document.body.onload = function(){
-  setTimeout(function() {
-    if(!preloader.classList.contains('done')){
+document.body.onload = function () {
+  setTimeout(function () {
+    if (!preloader.classList.contains('done')) {
       preloader.classList.add('done');
     }
   }, 500);
 }
 
-if(document.querySelector('.form')){
+if (document.querySelector('.form')) {
   const formContainer = document.querySelector('.container-form');
 
   const formLogin = document.querySelector('.form__login');
   const formSignUp = document.querySelector('.form__sign-up');
 
+  //Login
   const username = document.querySelector('#username');
   const password = document.querySelector('#password');
   const usernameError = document.querySelector('#username-error');
   const passwordError = document.querySelector('#password-error');
 
+  //Sign Up
   const usernameSignUp = document.querySelector('#username-signUp');
   const passwordSignUp = document.querySelector('#password-signUp');
   const confirmPassword = document.querySelector('#confirm-password');
@@ -35,32 +38,13 @@ if(document.querySelector('.form')){
   const linkToSignUp = document.querySelector('#link-to-signUp');
   const linkToLogin = document.querySelector('#link-to-login');
 
-  username.addEventListener('input', username_Verify);
-  password.addEventListener('input', password_Verify);
-
-  usernameSignUp.addEventListener('input', username_Verify);
-  passwordSignUp.addEventListener('input', password_Verify);
+  usernameSignUp.addEventListener('input', hiddenError);
+  passwordSignUp.addEventListener('input', hiddenError);
   confirmPassword.addEventListener('input', confirmPassword_Verify);
 
-  function validated(name, pass, nameError, passError, confirm_pass = null, confirm_passError = null){
-    if(name.value.length < 4) {
-      name.style.borderColor = "red";
-      nameError.style.display = "block";
-      name.focus();
-
-      return false;
-    }
-
-    if(pass.value.length < 4) {
-      pass.style.borderColor = "red";
-      passError.style.display = "block";
-      pass.focus();
-
-      return false;
-    }
-  
-    if(confirm_pass){
-      if(confirm_pass.value !== pass.value){
+  function validated(pass, confirm_pass, confirm_passError) {
+    if (confirm_pass) {
+      if (confirm_pass.value !== pass.value) {
         confirm_pass.style.borderColor = "red";
         confirm_passError.style.display = "block";
         confirm_pass.focus();
@@ -72,28 +56,8 @@ if(document.querySelector('.form')){
     return true;
   }
 
-  function username_Verify(e){
-    if(e.target.value.length >= 4){
-      e.target.style.borderColor = "gray";
-      usernameError.style.display = "none";
-      usernameSignUpError.style.display = "none";
-
-      return true;
-    }
-  }
-
-  function password_Verify(e){
-    if(e.target.value.length >= 4){
-      e.target.style.borderColor = "gray";
-      passwordError.style.display = "none";
-      passwordSignUpError.style.display = "none";
-
-      return true;
-    }
-  }
-
-  function confirmPassword_Verify(e){
-    if(e.target.value === passwordSignUp.value){
+  function confirmPassword_Verify(e) {
+    if (e.target.value === passwordSignUp.value) {
       e.target.style.borderColor = "gray";
       confirmPasswordError.style.display = "none";
 
@@ -101,24 +65,12 @@ if(document.querySelector('.form')){
     }
   }
 
-  function sendRequest(method, url, body = null) {
-    return fetch(url, {
-      method: method,
-      body: body,
-    }).then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-  
-      return response.json().then(err => {
-        const e = new Error('Не працює');
-        e.data = err;
-        throw e;
-      })
-    })
+  function hiddenError(e){
+    e.target.style.borderColor = "gray";
+    e.target.nextElementSibling.style.display = "none";
   }
 
-  //Перехід між авторизацією та регістрацією
+  // Transition between Login and Sign Up
   linkToSignUp.addEventListener('click', (e) => {
     e.preventDefault();
 
@@ -131,87 +83,109 @@ if(document.querySelector('.form')){
     formContainer.classList.remove('active');
   })
 
-  //Авторизація
+  // Login
   formLogin.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    if(validated(username, password, usernameError, passwordError)){
-      let dataForm = new FormData();
-  
-      dataForm.append('username', username.value );
-      dataForm.append('password', password.value);
-      
-      if(preloader.classList.contains('done')){
-        preloader.classList.remove('done');
-      }
+    // let dataForm = new FormData();
 
-      sendRequest('POST', urlList.localServer, dataForm)
-        .then(data => {
-          setTimeout(function() {
-            if(!preloader.classList.contains('done')){
-              preloader.classList.add('done');
-            }
-          }, 100);
+    // dataForm.append('username', username.value);
+    // dataForm.append('password', password.value);
 
-          console.log(data);
+    // if (preloader.classList.contains('done')) {
+    //   preloader.classList.remove('done');
+    // }
 
-          formContainer.style.display = 'none';
+    // sendRequest('POST', reqURL, dataForm)
+    //   .then(data => {
+    //     setTimeout(function () {
+    //       if (!preloader.classList.contains('done')) {
+    //         preloader.classList.add('done');
+    //       }
+    //     }, 100);
 
-          document.querySelector('.account__login-text').style.display = "block";
-        })
-        .catch(err => {
-          setTimeout(function() {
-            if(!preloader.classList.contains('done')){
-              preloader.classList.add('done');
-            }
-          }, 100);
-          
-          alert('Adding User failed');
-          console.log(err);
-        })
-    } else {
-      return false;
-    }
+    //     console.log(data);
+
+    //     formContainer.style.display = 'none';
+
+    //     document.querySelector('.account__login-text').style.display = "block";
+    //   })
+    //   .catch(err => {
+    //     setTimeout(function () {
+    //       if (!preloader.classList.contains('done')) {
+    //         preloader.classList.add('done');
+    //       }
+    //     }, 100);
+
+    //     alert('Adding User failed');
+    //     console.log(err);
+    //   })
   })
 
-  //Регістрація
+  // Sign Up
   formSignUp.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    if(validated(usernameSignUp, passwordSignUp, usernameSignUpError, passwordSignUpError, confirmPassword, confirmPasswordError)){
+    if (validated(passwordSignUp, confirmPassword, confirmPasswordError)) {
       let dataForm = new FormData();
-  
-      dataForm.append('username', usernameSignUp.value );
+
+      dataForm.append('username', usernameSignUp.value);
       dataForm.append('password', passwordSignUp.value);
-      
-      if(preloader.classList.contains('done')){
+
+      if (preloader.classList.contains('done')) {
         preloader.classList.remove('done');
       }
 
-      sendRequest('POST', urlList.localServer, dataForm)
-        .then(data => {
-          setTimeout(function() {
-            if(!preloader.classList.contains('done')){
+      async function getData(url, body) {
+        const response = await fetch(url, {
+          method: 'POST',
+          body: body
+        });
+        let data = await response.json();
+
+        if (response.ok) {
+          setTimeout(function () {
+            if (!preloader.classList.contains('done')) {
               preloader.classList.add('done');
             }
           }, 100);
-
-          console.log(data);
 
           formContainer.style.display = 'none';
 
           document.querySelector('.account__signup-text').style.display = "block";
-        })
-        .catch(err => {
-          setTimeout(function() {
-            if(!preloader.classList.contains('done')){
+        } else {
+          setTimeout(function () {
+            if (!preloader.classList.contains('done')) {
               preloader.classList.add('done');
             }
           }, 100);
 
-          alert('Adding User failed');
-          console.log(err);
-        })
+          console.log(data);
+
+          if (data.errors && data.message) {
+            if (data.errors.errors[0].path === 'username') {
+              usernameSignUp.style.borderColor = "red";
+              usernameSignUpError.style.display = "block";
+              usernameSignUpError.innerText = data.errors.errors[0].msg;
+              usernameSignUp.focus();
+
+            } else if (data.errors.errors[0].path === 'password') {
+              passwordSignUp.style.borderColor = "red";
+              passwordSignUpError.style.display = "block";
+              passwordSignUpError.innerText = data.errors.errors[0].msg;
+              passwordSignUp.focus();
+            };
+
+          } else if (data.message) {
+            usernameSignUp.style.borderColor = "red";
+            usernameSignUpError.style.display = "block";
+            usernameSignUpError.innerText = data.message;
+            usernameSignUp.focus();
+          }
+        }
+      }
+
+      getData(reqURL, dataForm);
 
     } else {
       return false;
