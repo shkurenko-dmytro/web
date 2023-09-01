be89408b0df7cd600133c6740e5c8d4b4031dadc
Maxim Kuts <makskutss@gmail.com>
2023-09-01 21:20:44 +0300

    react query

--- Kutzenko/React/query/src/App.tsx
+++ Kutzenko/React/query/src/App.tsx
@@ -1,29 +1,72 @@
-import { useQuery } from "@tanstack/react-query"
-import axios from "axios"
-import { ITodo } from "./app.interface"
-
-const todoId = 1
+import { useIsFetching, useMutation } from "@tanstack/react-query"
+import { useTodos } from "./hooks/useTodos"
+import { SyntheticEvent, useState } from "react"
+import todoService from "./services/todo.service"
 
 function App() {
-  const { isLoading, data } = useQuery(
-    ["todos", todoId],
-    () => axios.get<ITodo>("https://jsonplaceholder.typicode.com/todos/1"),
+  const { isLoading, data } = useTodos()
+
+  const countFetching = useIsFetching()
+
+  const [title, setTitle] = useState("")
+
+  const { mutate } = useMutation(
+    ["create todo"],
+    (title: string) => todoService.create(title),
     {
-      select: ({ data }) => data,
+      onSuccess() {
+        setTitle("")
+        alert("Todo created!")
+      },
     }
   )
 
+  const submitHandler = (e: SyntheticEvent) => {
+    e.preventDefault()
+    mutate(title)
+  }
+
   return (
-    <div>
-      {isLoading ? (
-        <div>Loading...</div>
-      ) : data ? (
-        <h1>Todo: {data.title}</h1>
-      ) : (
-        <h1>Data not found!</h1>
-      )}
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "1fr 1fr",
+        gap: 20,
+      }}
+    >
+      <div>
+        {!!countFetching && <h3>Loading...</h3>}
+        <h2>Create todo:</h2>
+        <form onSubmit={submitHandler}>
+          <div>
+            <input
+              type="text"
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
+              placeholder="Enter todo title"
+              style={{ marginBottom: 10 }}
+            />
+          </div>
+          <button>Create</button>
+        </form>
+      </div>
+      <div>
+        <h1>TODOS: </h1>
+
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : data?.length ? (
+          data.map((todo) => (
+            <div key={todo.id}>
+              <b>{todo.id}:</b> {todo.title}
+            </div>
+          ))
+        ) : (
+          <h1>Data not found!</h1>
+        )}
+      </div>
     </div>
   )
 }
 
-export default App
+export { App }
--- Kutzenko/React/query/src/services/todo.service.ts
+++ Kutzenko/React/query/src/services/todo.service.ts
@@ -1,5 +1,23 @@
-class TodoService {
+import axios from "axios"
+import { ICreateTodo, ITodo } from "../app.interface"
 
+class TodoService {
+  private URL = "https://jsonplaceholder.typicode.com/todos"
+
+  async getById(id: string) {
+    return axios.get<ITodo>(`${this.URL}/${id}`)
+  }
+  async getAll() {
+    return axios.get<ITodo[]>(`${this.URL}/?_start=0&_limit=5`)
+  }
+
+  async create(title: string){
+    return axios.post<any, any, ICreateTodo>(this.URL, {
+      title,
+      userId: 1,
+      completed: false
+    })
+  }
 }
 
 export default new TodoService()
