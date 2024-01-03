e209a6d5e50ade5d6307d7a62b256a72a8720a24
Maxim Kuts <makskutss@gmail.com>
2024-01-03 20:47:10 +0200

    Moved to components realization of adding ticker

---
+++ Kutzenko/Vue/vue-1/src/components/AddTicker.vue
@@ -0,0 +1,57 @@
+<template>
+  <section>
+    <div class="flex">
+      <div class="max-w-xs">
+        <label for="wallet" class="block text-sm font-medium text-gray-700">Тикер</label>
+        <div class="mt-1 relative rounded-md shadow-md">
+          <input
+            v-model="ticker"
+            @keyup.enter="addTicker"
+            type="text"
+            name="wallet"
+            id="wallet"
+            class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
+            placeholder="Например DOGE"
+          />
+        </div>
+        <!-- <div v-if="filteredHints.length > 0" class="flex bg-white shadow-md p-1 rounded-md flex-wrap">
+        <span
+          v-for="hint in filteredHints"
+          :key="hint.id"
+          @click="(this.ticker = hint.Symbol), addTicker()"
+          class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
+        >
+          {{ hint.Symbol }}
+        </span>
+      </div>
+      <div v-if="checkedTicker" class="text-sm text-red-600">Такой тикер уже добавлен</div> -->
+      </div>
+    </div>
+    <add-button @add="addTicker" />
+  </section>
+</template>
+
+<script>
+import AddButton from '@/components/AddButton.vue'
+
+export default {
+  components: {
+    AddButton
+  },
+
+  data() {
+    return {
+      ticker: ''
+    }
+  },
+
+  methods: {
+    addTicker() {
+      this.$emit('add-ticker', this.ticker)
+      this.ticker = ''
+    }
+  }
+}
+</script>
+
+<style scoped></style>
--- Kutzenko/Vue/vue-1/src/pages/HomePage.vue
+++ Kutzenko/Vue/vue-1/src/pages/HomePage.vue
@@ -26,57 +26,7 @@
       </svg>
     </div>
     <div class="container">
-      <section>
-        <div class="flex">
-          <div class="max-w-xs">
-            <label for="wallet" class="block text-sm font-medium text-gray-700">Тикер</label>
-            <div class="mt-1 relative rounded-md shadow-md">
-              <input
-                v-model="ticker"
-                @keyup.enter="addTicker"
-                type="text"
-                name="wallet"
-                id="wallet"
-                class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
-                placeholder="Например DOGE"
-              />
-            </div>
-            <div
-              v-if="filteredHints.length > 0"
-              class="flex bg-white shadow-md p-1 rounded-md flex-wrap"
-            >
-              <span
-                v-for="hint in filteredHints"
-                :key="hint.id"
-                @click="(this.ticker = hint.Symbol), addTicker()"
-                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
-              >
-                {{ hint.Symbol }}
-              </span>
-            </div>
-            <div v-if="checkedTicker" class="text-sm text-red-600">Такой тикер уже добавлен</div>
-          </div>
-        </div>
-        <button
-          @click="addTicker"
-          type="button"
-          class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-        >
-          <svg
-            class="-ml-0.5 mr-2 h-6 w-6"
-            xmlns="http://www.w3.org/2000/svg"
-            width="30"
-            height="30"
-            viewBox="0 0 24 24"
-            fill="#ffffff"
-          >
-            <path
-              d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
-            ></path>
-          </svg>
-          Добавить
-        </button>
-      </section>
+      <add-ticker @add-ticker="addTicker" />
       <button
         v-if="page > 1"
         @click="page = Number(page) - 1"
@@ -186,11 +136,15 @@
 <script>
 import { subscribeToTicker, unSubscribeToTicker, getAllCoins } from '@/api'
 import { getFromStorage, saveToStorage } from '@/storageManager'
+import AddTicker from '@/components/AddTicker.vue'
 
 export default {
+  components: {
+    AddTicker
+  },
+
   data() {
     return {
-      ticker: '',
       tickers: [],
       maxTickersOnPage: 6,
 
@@ -262,19 +216,19 @@ export default {
       return this.graph.map((price) => 5 + ((price - minValue) * 95) / (maxValue - minValue))
     },
 
-    checkedTicker() {
-      return this.tickers.find((t) => t.name.toUpperCase() === this.ticker.toUpperCase())
-    },
+    // checkedTicker() {
+    //   return this.tickers.find((t) => t.name.toUpperCase() === this.ticker.toUpperCase())
+    // },
 
-    filteredHints() {
-      return this.coinList
-        .filter(
-          (t) =>
-            t.Symbol.toUpperCase().includes(this.ticker.toUpperCase()) &&
-            this.ticker.toUpperCase() !== ''
-        )
-        .slice(0, 4)
-    },
+    // filteredHints() {
+    //   return this.coinList
+    //     .filter(
+    //       (t) =>
+    //         t.Symbol.toUpperCase().includes(this.ticker.toUpperCase()) &&
+    //         this.ticker.toUpperCase() !== ''
+    //     )
+    //     .slice(0, 4)
+    // },
 
     savedHistoryOptions() {
       return {
@@ -315,23 +269,23 @@ export default {
       return price > 1 ? price.toFixed(2) : price.toPrecision(2)
     },
 
-    addTicker() {
+    addTicker(ticker) {
       const currentTicker = {
-        name: this.ticker.toUpperCase(),
+        name: ticker.toUpperCase(),
         price: '-',
         isValidTicker: true,
         symbolPrice: 'USD'
       }
 
-      if (!this.checkedTicker) {
-        this.tickers = [...this.tickers, currentTicker]
-        this.ticker = ''
-        this.filter = ''
+      this.tickers = [...this.tickers, currentTicker]
+      this.filter = ''
 
-        subscribeToTicker(currentTicker.name, (newPrice, isValidSub, symbolPrice) => {
-          this.updateTicker(currentTicker.name, newPrice, isValidSub, symbolPrice)
-        })
-      }
+      subscribeToTicker(currentTicker.name, (newPrice, isValidSub, symbolPrice) => {
+        this.updateTicker(currentTicker.name, newPrice, isValidSub, symbolPrice)
+      })
+
+      // if (!this.checkedTicker) {
+      // }
     },
 
     handleRemove(tickerToRemove) {
