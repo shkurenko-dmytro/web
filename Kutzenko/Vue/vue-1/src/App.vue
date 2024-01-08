0f9088a61bd135185c2f213b877c1fbc5e44ae88
Maxim Kuts <makskutss@gmail.com>
2024-01-08 21:29:37 +0200

    Practice with slots, did modal window #1

--- Kutzenko/Vue/vue-1/src/App.vue
+++ Kutzenko/Vue/vue-1/src/App.vue
@@ -1,9 +1,27 @@
 <template>
-  <router-view></router-view>
+  <!-- <router-view></router-view> -->
+  <modal-window v-if="isOpen" @click="isOpen = false"
+    ><template #main="{ item }">
+      <div>Доброго ранку, {{ item }}</div> </template
+    ><template #footer>
+      <div><input type="text" /> <button type="button" @click="isOpen = false">OK</button></div>
+    </template></modal-window
+  >
 </template>
 
 <script>
-export default {}
+import ModalWindow from './components/ModalWindow.vue'
+export default {
+  components: {
+    ModalWindow
+  },
+
+  data() {
+    return {
+      isOpen: true
+    }
+  }
+}
 </script>
 
 <style scoped></style>
---
+++ Kutzenko/Vue/vue-1/src/components/ModalWindow.vue
@@ -0,0 +1,44 @@
+<template>
+  <div v-if="isOpen" class="modal" @click="$emit('click')">
+    <div @click.stop class="modal__content">
+      <slot name="main" :item="item"></slot>
+      <slot name="footer"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpen: true,
+      item: 'Max'
+    }
+  },
+
+  emits: {
+    click: null
+  }
+}
+</script>
+
+<style scoped>
+.modal {
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  display: flex;
+}
+
+.modal__content {
+  margin: auto;
+  background: white;
+  border-radius: 12px;
+  min-height: 50px;
+  min-width: 300px;
+  padding: 20px;
+}
+</style>
